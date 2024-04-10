6a2af70e4c11c7dbfc7146430bdca1b705f0750b
nadim obeid <nadim.obeid@lau.edu>
2024-04-10 21:43:36 +0300

    fixed some bugs in omp code

--- PSO_CUDA.c
+++ PSO_CUDA.c
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define N_PARTICLES 20
+#define N_PARTICLES 1024
 #define W 0.8
 #define C1 0.1
 #define C2 0.1
-#define N_ITERATIONS 50
+#define N_ITERATIONS 500
 
 typedef struct{
     float x_pos;
@@ -26,27 +26,32 @@ double f(double x, double y) {
 
 
 // CUDA kernel to update particle positions and velocities
-_global_ void update(double *X, double *V, double *pbest, double *pbest_obj, double *gbest, double *gbest_obj) {
+__global__ void update(particle *particles, double *gbest, double *gbest_obj) {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     if (i < N_PARTICLES) {
-        double r1 = (double)rand() / RAND_MAX;
-        double r2 = (double)rand() / RAND_MAX;
+        for(int k = 0; k<N_ITERATIONS; k++){
+            double r1 = (double)rand() / RAND_MAX;
+            double r2 = (double)rand() / RAND_MAX;
 
-        V[i] = W * V[i] + C1 * r1 * (pbest[i] - X[i]) + C2 * r2 * (gbest[i / 2] - X[i]);
+            particles[i].x_velo = W * particles[i].x_velo + C1 * r1 * (particles[i].x_best - particles[i].x_pos) + C2 * r2 * (gbest[0] - particles[i].x_pos);
+            particles[i].y_velo = W * particles[i].y_velo + C1 * r1 * (particles[i].y_best - particles[i].y_pos) + C2 * r2 * (gbest[1] - particles[i].y_pos);
 
-        X[i] += V[i];
+            particles[i].x_pos += particles[i].x_velo;
+            particles[i].y_pos += particles[i].y_velo;
 
-        double obj = f(X[i],X[i+N_PARTICLES]);
-        if (pbest_obj[i] > obj) {
-            pbest[i] = X[i];
-            pbest_obj[i] = obj;
-        }
+            double obj = f(particles[i].x_best,particles[i].y_best);
+            if (particles[i].best > obj) {
+                particles[i].x_best = particles[i].x_pos;
+                particles[i].y_best = particles[i].y_pos;
+                particles[i].best = obj;
+            }
 
-        __syncthreads();
-
-        if (pbest_obj[i] < *gbest_obj) {
-            atomicMin((unsigned long long int *)gbest_obj, (unsigned long long int)pbest_obj[i]);
-            gbest[i / 2] = pbest[i];
+            if (particles[i].best < *gbest_obj) {
+                atomicMin((unsigned long long int *)gbest_obj, (unsigned long long int)particles[i].best);
+                gbest[0] = particles[i].x_best;
+                gbest[1] = particles[i].y_best;
+            }
+            __syncthreads();
         }
     }
 }
@@ -55,57 +60,45 @@ int main() {
     srand(time(NULL));
 
     // Initialize host variables
-    double X_host[2 * N_PARTICLES];
-    double V_host[2 * N_PARTICLES];
-    double pbest_host[2 * N_PARTICLES];
-    double pbest_obj_host[N_PARTICLES];
+    particle particles_host[N_PARTICLES];
     double gbest_host[2];
     double gbest_obj_host = INFINITY;
 
     // Initialize device variables
-    double *X_dev, *V_dev, *pbest_dev, *pbest_obj_dev, *gbest_dev, *gbest_obj_dev;
-    cudaMalloc((void **)&X_dev, 2 * N_PARTICLES * sizeof(double));
-    cudaMalloc((void **)&V_dev, 2 * N_PARTICLES * sizeof(double));
-    cudaMalloc((void **)&pbest_dev, 2 * N_PARTICLES * sizeof(double));
-    cudaMalloc((void **)&pbest_obj_dev, N_PARTICLES * sizeof(double));
+    particle *particles_dev;
+    double *gbest_dev, *gbest_obj_dev;
+    cudaMalloc((void **)&particles_dev, N_PARTICLES * sizeof(particle));
     cudaMalloc((void **)&gbest_dev, 2 * sizeof(double));
     cudaMalloc((void **)&gbest_obj_dev, sizeof(double));
 
     // Initialize particles
     for (int i = 0; i < N_PARTICLES; i++) {
-        X_host[i] = (double)rand() / RAND_MAX * 5;
-        X_host[i + N_PARTICLES] = (double)rand() / RAND_MAX * 5;
-        V_host[i] = (double)rand() / RAND_MAX * 0.1;
-        V_host[i + N_PARTICLES] = (double)rand() / RAND_MAX * 0.1;
-        pbest_host[i] = X_host[i];
-        pbest_host[i + N_PARTICLES] = X_host[i + N_PARTICLES];
-        pbest_obj_host[i] = pow(X_host[i] - 3.14, 2) + pow(X_host[i + N_PARTICLES] - 2.72, 2) + sin(3 * X_host[i] + 1.41) + sin(4 * X_host[i + N_PARTICLES] - 1.73);
+        particles_host[i].x_pos = (double)rand() / RAND_MAX * 5;
+        particles_host[i].y_pos = (double)rand() / RAND_MAX * 5;
+        particles_host[i].x_velo = (double)rand() / RAND_MAX * 0.1;
+        particles_host[i].y_velo = (double)rand() / RAND_MAX * 0.1;
+        particles_host[i].x_best = particles_host[i].x_pos;
+        particles_host[i].y_best = particles_host[i].y_pos;
+        particles_host[i].best= f(particles_host[i].x_best,particles[i].y_best);
     }
 
     // Copy data from host to device
-    cudaMemcpy(X_dev, X_host, 2 * N_PARTICLES * sizeof(double), cudaMemcpyHostToDevice);
-    cudaMemcpy(V_dev, V_host, 2 * N_PARTICLES * sizeof(double), cudaMemcpyHostToDevice);
-    cudaMemcpy(pbest_dev, pbest_host, 2 * N_PARTICLES * sizeof(double), cudaMemcpyHostToDevice);
-    cudaMemcpy(pbest_obj_dev, pbest_obj_host, N_PARTICLES * sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(particles_dev, particles_host, N_PARTICLES * sizeof(particle), cudaMemcpyHostToDevice);
     cudaMemcpy(gbest_dev, gbest_host, 2 * sizeof(double), cudaMemcpyHostToDevice);
     cudaMemcpy(gbest_obj_dev, &gbest_obj_host, sizeof(double), cudaMemcpyHostToDevice);
 
     // PSO iterations
-    for (int i = 0; i < N_ITERATIONS; i++) {
-        update<<<(N_PARTICLES + 255) / 256, 256>>>(X_dev, V_dev, pbest_dev, pbest_obj_dev, gbest_dev, gbest_obj_dev);
-    }
+    
+    update<<<(N_PARTICLES + 255) / 256, 256>>>(particles_dev, gbest_dev, gbest_obj_dev);
 
     // Copy data from device to host
     cudaMemcpy(&gbest_host, gbest_dev, 2 * sizeof(double), cudaMemcpyDeviceToHost);
     cudaMemcpy(&gbest_obj_host, gbest_obj_dev, sizeof(double), cudaMemcpyDeviceToHost);
 
-    printf("PSO found best solution at f(%lf,%lf)=%lf\n", gbest_host[0], gbest_host[1], gbest_obj_host);
+    printf("PSO found best solution at f(%f,%f)=%f\n", gbest_host[0], gbest_host[1], gbest_obj_host);
 
     // Free device memory
-    cudaFree(X_dev);
-    cudaFree(V_dev);
-    cudaFree(pbest_dev);
-    cudaFree(pbest_obj_dev);
+    cudaFree(particles_dev);
     cudaFree(gbest_dev);
     cudaFree(gbest_obj_dev);
 
--- PSO_OMP.c
+++ PSO_OMP.c
@@ -8,10 +8,10 @@
 #define W 0.8
 #define C1 0.1
 #define C2 0.1
-#define N_ITERATIONS 500
-#define N 4
+#define N_ITERATIONS 50
+#define N 5
 
-typedef struct{
+typedef struct {
     float x_pos;
     float y_pos;
     float x_velo;
@@ -19,81 +19,70 @@ typedef struct{
     float x_best;
     float y_best;
     float best;
+} particle;
 
-}particle;
-
-// function we need to find the global minimum for.
+// Function we need to find the global minimum for.
 double f(double x, double y) {
     return pow(x - 3.14, 2) + pow(y - 2.72, 2) + sin(3 * x + 1.41) + sin(4 * y - 1.73);
 }
 
-void update(particle particles[N_PARTICLES], double gbest[2], double* gbest_obj) {
-    double r1 = (double)rand() / RAND_MAX;
-    double r2 = (double)rand() / RAND_MAX;
-    particle *curr_particle; 
-    #pragma omp parallel for num_threads (N) private(curr_particle) shared(particles) schedule(static, N_PARTICLES/N)
-    for (int i = 0; i < N_PARTICLES; i++) {
-        curr_particle = &particles[i];
-        curr_particle->x_velo = W * curr_particle->x_velo + C1 * r1 * (curr_particle->x_best - curr_particle->x_pos) + C2 * r2 * (gbest[0] - curr_particle->x_pos);
-        curr_particle->y_velo = W * curr_particle->y_velo + C1 * r1 * (curr_particle->y_best -curr_particle->y_pos) + C2 * r2 * (gbest[1] -curr_particle->y_pos);
+void update_particle(particle *curr_particle, double gbest[2], double *gbest_obj, double r1, double r2) {
+    curr_particle->x_velo = W * curr_particle->x_velo + C1 * r1 * (curr_particle->x_best - curr_particle->x_pos) + C2 * r2 * (gbest[0] - curr_particle->x_pos);
+    curr_particle->y_velo = W * curr_particle->y_velo + C1 * r1 * (curr_particle->y_best - curr_particle->y_pos) + C2 * r2 * (gbest[1] - curr_particle->y_pos);
 
-        curr_particle->x_pos += curr_particle->x_velo;
-        curr_particle->y_pos += curr_particle->y_velo;
+    curr_particle->x_pos += curr_particle->x_velo;
+    curr_particle->y_pos += curr_particle->y_velo;
 
-        double obj = f(curr_particle->x_pos,curr_particle->y_pos);
-        if (curr_particle->best > obj) {
-            curr_particle->x_best = curr_particle->x_pos;
-            curr_particle->y_best =curr_particle->y_pos;
-            curr_particle->best = obj;
+    double obj = f(curr_particle->x_pos, curr_particle->y_pos);
+    if (curr_particle->best > obj) {
+        curr_particle->x_best = curr_particle->x_pos;
+        curr_particle->y_best = curr_particle->y_pos;
+        curr_particle->best = obj;
+        // Update global best
+        #pragma omp critical
+        {
+            if (obj < *gbest_obj) {
+                *gbest_obj = obj;
+                gbest[0] = curr_particle->x_pos;
+                gbest[1] = curr_particle->y_pos;
+            }
         }
-    }
-
-    int min_index = 0;
-    for (int i = 1; i < N_PARTICLES; i++) {
-        if (particles[i].best < particles[min_index].best) {
-            min_index = i;
-        }
-    }
-
-    if (particles[min_index].best < *gbest_obj) {
-        *gbest_obj = particles[min_index].best;
-        gbest[0] = particles[min_index].x_best;
-        gbest[1] = particles[min_index].y_best;
     }
 }
 
 int main() {
     srand(time(NULL));
-    // Initialize particles
-    particle particles [N_PARTICLES];
+    particle particles[N_PARTICLES];
     double gbest[2];
     double gbest_obj = INFINITY;
     double total_time;
-   
 
-    // initialize particles
-    for(int k = 0; k<10; k++){
-        clock_t start = clock();
-        for (int i = 0; i < N_PARTICLES; i++) {
-            particles[i].x_pos = (double)rand() / RAND_MAX * 5;
-            particles[i].y_pos = (double)rand() / RAND_MAX * 5;
-            particles[i].x_velo = (double)rand() / RAND_MAX * 0.1;
-            particles[i].y_velo = (double)rand() / RAND_MAX * 0.1;
-            particles[i].x_best = particles[i].x_pos;
-            particles[i].y_best =particles[i].y_pos;
-            particles[i].best = f(particles[i].x_pos,particles[i].y_pos);
-        }
-
-
-        // PSO iterations
-        for (int i = 0; i < N_ITERATIONS; i++) {
-            update(particles, gbest, &gbest_obj);
-        }
-        clock_t end = clock();
-        total_time += (double)(end - start) / CLOCKS_PER_SEC;
+    clock_t start = clock();
+    #pragma omp parallel for
+    for (int i = 0; i < N_PARTICLES; i++) {
+        particles[i].x_pos = (double)rand() / RAND_MAX * 5;
+        particles[i].y_pos = (double)rand() / RAND_MAX * 5;
+        particles[i].x_velo = (double)rand() / RAND_MAX * 0.1;
+        particles[i].y_velo = (double)rand() / RAND_MAX * 0.1;
+        particles[i].x_best = particles[i].x_pos;
+        particles[i].y_best = particles[i].y_pos;
+        particles[i].best = f(particles[i].x_pos, particles[i].y_pos);
     }
+
+    // PSO iterations
+    for (int i = 0; i < N_ITERATIONS; i++) {
+        double r1 = (double)rand() / RAND_MAX;
+        double r2 = (double)rand() / RAND_MAX;
+        #pragma omp parallel for
+        for (int j = 0; j < N_PARTICLES; j++) {
+            update_particle(&particles[j], gbest, &gbest_obj, r1, r2);
+        }
+    }
+    clock_t end = clock();
+    total_time = (double)(end - start) / CLOCKS_PER_SEC;
+
     printf("PSO found best solution at f(%lf,%lf)=%lf\n", gbest[0], gbest[1], gbest_obj);
-    printf("Average time is: %f s\n", total_time/10);
+    printf("Average time is: %f s\n", total_time);
 
     return 0;
 }
